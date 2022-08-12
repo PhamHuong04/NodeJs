85589e4e679be96dd0964ce87aea0b29149c0779
Pham Huong <phamhuong240601@gmail.com>
2022-08-12 15:40:12 +0700

    update query 3

--- server.js
+++ server.js
@@ -30,12 +30,67 @@ app.use('/permission', permissionRouter);
 app.get('/user-query', async (req, res) => {
     const page = 1
     const limit = 5;
-    const skip = (page - 1) * limit
-    const users = await UserModel.find(req.query).skip(skip).limit(limit)
+    const skip = (page - 1) * limit;
+    const users = await UserModel.find(req.query).skip(skip).limit(limit);
 
     res.json({
         users
     })
+})
+
+// Lấy ra các thuộc tính name, address, age của tất cả users có tuổi lớn hơn 20 và sống tại quận Hà Đông, tính tổng documents, có phân trang.
+app.get("/user-age-and-address", async (req, res) => {
+    const page = 1;
+    const perPage = 5;
+    const skip = (page -1)* perPage;
+    const users = await UserModel
+        .find(
+            {
+                age: {
+                    $gt: 20,
+                },
+
+            },
+            {
+                address: {
+                    $search: ['Ha Dong'],
+                },
+            },
+        )
+        .select('name age address')
+        .limit(perPage)
+        .skip(skip);
+    
+        res.json({
+            data: {
+                results: users.length,
+                users,
+            }
+        })
+})
+
+
+app.get('/all-user-and-role-permission', async(req, res)=>{
+    const page = 1;
+    const perPage = 5;
+    const skip = (page - 1) * perPage;
+    const users = UserModel.aggregate([
+        {
+            $lookup:
+            {
+                from: "permission",
+                localField: "role",
+                foreignField: "_id",
+                as: "role-permisson"
+            }
+        }
+    ])
+    res.json({
+        data: {
+            users,
+        }
+    })
+
 })
 
 
